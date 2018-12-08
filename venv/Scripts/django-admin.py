09d700b51dbfaf5d6406ef6864f1c373e4e67738
joaonunovalente <jnvalente@ua.pt>
2018-12-08 23:44:56 +0000

    First Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\GreenLock\PycharmProjects\PlatypusProject\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
